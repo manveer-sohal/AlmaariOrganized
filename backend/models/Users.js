06163dfd9fd634eb1e4bfbea17dce6954910738d
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-09-23 23:25:56 -0400

    updated schema to include various tags

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -7,12 +7,26 @@ const ClothesSchema = new mongoose.Schema({
   favourite: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   colour: { type: [String], required: true },
+  seaon: { type: [String], default: [] },
+  waterproof: { type: Boolean, default: false },
+});
+
+const OutfitsSchema = new mongoose.Schema({
+  uniqueId: { type: String, required: true, unique: true },
+  name: { type: String, default: "" },
+  favourite: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+  colour: { type: [String], required: true },
+  seaon: { type: [String], default: [] },
+  waterproof: { type: Boolean, default: false },
+  outfit_items: { type: [ClothesSchema], default: [] },
 });
 
 const usersSchema = new mongoose.Schema({
   auth0Id: { type: String, required: true, unique: true, index: true }, // Link to Auth0 user ID
   email: { type: String, required: true },
   Clothes: { type: [ClothesSchema], default: [] },
+  outfits: { type: [OutfitsSchema], default: [] },
 });
 
 const User = mongoose.models.User || mongoose.model("User", usersSchema);
