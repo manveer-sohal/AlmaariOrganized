d0c9194f28ae0f0c2028cbea277e6e9929ffba9a
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-09-23 23:25:23 -0400

    added a concrete time scale for weather

--- backend/controllers/weatherController.js
+++ backend/controllers/weatherController.js
@@ -36,14 +36,8 @@ async function getWeatherData(long, lat) {
 
   // configure the time frame up to 6 hours back and 15 days out
   const now = moment.utc();
-  const startTime = moment
-    .utc(now)
-    .add(0, "minutes")
-    .toISOString();
-  const endTime = moment
-    .utc(now)
-    .add(1, "days")
-    .toISOString();
+  const startTime = moment.utc(now).add(0, "minutes").toISOString();
+  const endTime = moment.utc(now).add(1, "days").toISOString();
 
   // specify the timezone, using standard IANA timezone format
   const timezone = "America/New_York";
