cd394ab37855ebab375c03a8861f8d1b60ac5cb4
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-09-25 21:31:49 -0400

    added new db route to get outfits

--- backend/routes/clothesRoutes.js
+++ backend/routes/clothesRoutes.js
@@ -5,16 +5,16 @@ import {
   removeData,
   uploadMiddleware,
   createOutfit,
+  getOutfits,
 } from "../controllers/clothesController.js";
 
 const router = express.Router();
 
 // Define routes
-
+router.post("/getOutfits", getOutfits);
 router.post("/listClothes", getData);
 router.post("/upload", uploadMiddleware, uploadData);
 router.post("/remove", removeData);
-
 router.post("/createOutfit", createOutfit);
 
 export default router;
