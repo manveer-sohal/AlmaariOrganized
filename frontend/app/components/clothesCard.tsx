0d5cf4c031f170f4263e68ce5519f5e045d627e6
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-10-08 11:29:13 -0400

    added loading animation for deletion

--- frontend/app/components/clothesCard.tsx
+++ frontend/app/components/clothesCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-type ClothingItemProps = {
+export type ClothingItemProps = {
   colour: string[];
   type: string;
   imageSrc: string;
@@ -16,10 +16,12 @@ export default function ClothesCard({
   onDelete,
 }: ClothingItemProps) {
   const [click, setClick] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const { user } = useUser(); // Auth0 user information
   const API_BASE_URL =
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
   const removeClothingItem = async () => {
+    setLoading(true);
     if (!user) {
       console.error("User is not authenticated. Cannot remove clothing item.");
       return;
@@ -38,6 +40,7 @@ export default function ClothesCard({
       if (response.ok) {
         console.log("Clothing item removed successfully:", data);
         onDelete();
+        setLoading(false);
       } else {
         console.error("Error removing clothing item:", data.error);
       }
@@ -49,13 +52,17 @@ export default function ClothesCard({
   return (
     <div className="border border-indigo-300 m-2 p-2 bg-slate-100 rounded-md w-[200px] h-[200px] shadow-lg relative overflow-hidden cursor-pointer transition-transform ease-in-out duration-300 hover:scale-105 hover:shadow-2xl">
       <div onClick={() => setClick(!click)} className="h-full w-full">
-        <Image
-          src={imageSrc}
-          alt="Clothing item"
-          width={200}
-          height={200}
-          className="object-contain w-full h-full absolute inset-0"
-        />
+        {loading ? (
+          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-indigo-500 absolute inset-0 m-auto"></div>
+        ) : (
+          <Image
+            src={imageSrc}
+            alt="Clothing item"
+            width={200}
+            height={200}
+            className="object-contain w-full h-full absolute inset-0"
+          />
+        )}
       </div>
       {click && (
         <button
