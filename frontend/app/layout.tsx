7a1f1809d03b8a0e8035aa7c4d4d502178a13cfa
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-12-04 22:19:51 -0500

    Add QueryClientProvider to RootLayout for @tanstack/react-query integration, enabling global data fetching capabilities.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,9 @@
 "use client";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
+
 import "./globals.css";
 
 //example of a dataset of clothes, the mao function will load the clothesCard component 3 times, filling in the prop variables
@@ -11,9 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <UserProvider>
-      <html lang="en">
-        <body className="text-gray-900 button-">{children}</body>
-      </html>
+      <QueryClientProvider client={client}>
+        <html lang="en">
+          <body className="text-gray-900 button-">{children}</body>
+        </html>
+      </QueryClientProvider>
     </UserProvider>
   );
 }
