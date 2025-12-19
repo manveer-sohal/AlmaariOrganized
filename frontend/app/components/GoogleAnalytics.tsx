14ac2dc874fa707ed1a3b5f8642378f2d6dace13
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-12-19 08:31:11 -0500

    moved google analytics to serperate component

---
+++ frontend/app/components/GoogleAnalytics.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-4KCQ0WMKMY"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4KCQ0WMKMY');
+        `}
+      </Script>
+    </>
+  );
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,40 +1,25 @@
-"use client";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Script from "next/script";
+import "./globals.css";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const client = new QueryClient();
 
-import "./globals.css";
-
-//example of a dataset of clothes, the mao function will load the clothesCard component 3 times, filling in the prop variables
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <UserProvider>
-      <QueryClientProvider client={client}>
-        <html lang="en">
-          <head>
-            <Script
-              src="https://www.googletagmanager.com/gtag/js?id=G-4KCQ0WMKMY"
-              strategy="afterInteractive"
-            />
-            <Script id="gtag-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-4KCQ0WMKMY');
-              `}
-            </Script>
-          </head>
-          <body className="text-gray-900 button-">{children}</body>
-        </html>
-      </QueryClientProvider>
-    </UserProvider>
+    <html lang="en">
+      <head>
+        <GoogleAnalytics />
+      </head>
+      <body className="text-gray-900">
+        <UserProvider>
+          <QueryClientProvider client={client}>{children}</QueryClientProvider>
+        </UserProvider>
+      </body>
+    </html>
   );
 }
