08517eb24063177ebe765e32b66ac5f1044babcd
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-12-30 15:11:15 -0500

    Add onboarding tour components for clothing and outfit creation, including step-by-step guidance with popovers.

---
+++ frontend/app/components/OnBoardingTourOutfit.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { driver, Driver } from "driver.js";
+
+let tour: Driver | null = null;
+
+export function startOnboardingTourOutfit() {
+  tour = driver({
+    showProgress: true,
+    steps: [
+      {
+        element: "#create-outfit-btn",
+        popover: {
+          title: "Create an outfit",
+          description: "Click this button to create an outfit.",
+          showButtons: [],
+        },
+      },
+      {
+        element: "#create-outfit-form",
+        popover: {
+          title: "Build From Your Wardrobe",
+          description:
+            "Click on the items in your wardrobe to add them to your outfit.",
+        },
+      },
+
+      {
+        element: "#outfit-preview",
+        popover: {
+          title: "Preview your outfit!",
+          description:
+            "Preview your outfit to see how it looks. Clothes can be layered and they can beremoved when clicked.",
+        },
+      },
+      {
+        element: "#ai-stylist",
+        popover: {
+          title: "Say Hi to your AI Stylist!",
+          description:
+            "An AI Stylist is available to help you create an outfit. Providing a tips on how to improve your outfit.",
+        },
+      },
+      {
+        element: "#create-outfit-form-name",
+        popover: {
+          title: "Give your outfit a name!",
+          description: "Finally, give your outfit a name and save it!",
+        },
+      },
+    ],
+  });
+
+  tour.drive();
+}
+
+export function goToNextTourStepOutfit() {
+  tour?.moveNext();
+}
