e69917599a1d8f27f098be784484941dbd3bf60e
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2026-01-10 23:50:45 -0500

    Enhance onboarding tour functionality: adapt button selectors for mobile and desktop, improve step navigation with loading checks, and streamline outfit creation descriptions for better user guidance.

--- frontend/app/components/OnBoardingTour.tsx
+++ frontend/app/components/OnBoardingTour.tsx
@@ -1,27 +1,33 @@
 "use client";
 import { driver, Driver } from "driver.js";
+import { isMobile } from "react-device-detect";
 
 let tour: Driver | null = null;
 
-export function startOnboardingTour() {
+export async function startOnboardingTour() {
+  const addButtonSelector = isMobile
+    ? "#add-clothes-btn-mobile"
+    : "#add-clothes-btn-desktop";
+
   tour = driver({
     showProgress: true,
-    allowClose: false,
-    disableActiveInteraction: false, // IMPORTANT
+
     steps: [
       {
-        element: "#add-clothes-btn",
+        element: addButtonSelector,
+
         popover: {
           title: "Welcome to the app! Add your first item",
           description: "Click this button to add your first item.",
           showButtons: [],
+          side: "left",
         },
       },
       {
-        element: "#add-clothes-form",
         popover: {
           title: "Fill in the form",
           description: "Fill in the form to add your clothing item.",
+          side: "left",
           showButtons: ["close"],
         },
       },
@@ -32,5 +38,13 @@ export function startOnboardingTour() {
 }
 
 export function goToNextTourStep() {
-  tour?.moveNext();
+  //check if  id="add-clothes-form" has loaded
+  const addClothesForm = document.getElementById("add-clothes-form");
+  if (addClothesForm) {
+    tour?.moveNext();
+  } else {
+    setTimeout(() => {
+      goToNextTourStep();
+    }, 100);
+  }
 }
--- frontend/app/components/OnBoardingTourOutfit.tsx
+++ frontend/app/components/OnBoardingTourOutfit.tsx
@@ -1,56 +1,70 @@
 "use client";
 import { driver, Driver } from "driver.js";
+import { isMobile } from "react-device-detect";
 
 let tour: Driver | null = null;
 
 export function startOnboardingTourOutfit() {
-  tour = driver({
-    showProgress: true,
-    steps: [
-      {
-        element: "#create-outfit-btn",
-        popover: {
-          title: "Create an outfit",
-          description: "Click this button to create an outfit.",
-          showButtons: [],
-        },
-      },
-      {
-        element: "#create-outfit-form",
-        popover: {
-          title: "Build From Your Wardrobe",
-          description:
-            "Click on the items in your wardrobe to add them to your outfit.",
-        },
-      },
+  let description = "";
+  let id = "";
+  if (isMobile) {
+    description =
+      'Scroll to the right and click the "Create Outfit" button to create an outfit.';
+    id = "#mobile-sidebar-ul";
+  } else {
+    description = "click this button to create an outfit.";
+    id = "#create-outfit-btn";
+  }
+  setTimeout(() => {
+    tour = driver({
+      showProgress: true,
+      steps: [
+        {
+          element: id,
 
-      {
-        element: "#outfit-preview",
-        popover: {
-          title: "Preview your outfit!",
-          description:
-            "Preview your outfit to see how it looks. Clothes can be layered and they can be removed when clicked.",
+          popover: {
+            title: "Create an outfit",
+            description: description,
+            showButtons: [],
+          },
         },
-      },
-      {
-        element: "#ai-stylist",
-        popover: {
-          title: "Say Hi to your AI Stylist!",
-          description:
-            "An AI Stylist is available to help you create an outfit. Providing a tips on how to improve your outfit.",
+        {
+          element: "#create-outfit-form",
+          popover: {
+            title: "Build From Your Wardrobe",
+            description:
+              "Click on the items in your wardrobe to add them to your outfit.",
+          },
         },
-      },
-      {
-        element: "#create-outfit-form-name",
-        popover: {
-          title: "Give your outfit a name!",
-          description: "Finally, give your outfit a name and save it!",
-        },
-      },
-    ],
-  });
 
-  tour.drive();
+        {
+          element: "#outfit-preview",
+          popover: {
+            title: "Preview your outfit!",
+            description:
+              "Preview your outfit to see how it looks. Clothes can be layered and they can be removed when clicked.",
+          },
+        },
+        {
+          element: "#ai-stylist",
+          popover: {
+            title: "Say Hi to your AI Stylist!",
+            description:
+              "An AI Stylist is available to help you create an outfit. Providing a tips on how to improve your outfit.",
+          },
+        },
+        {
+          element: "#create-outfit-form-name",
+          popover: {
+            title: "Give your outfit a name!",
+            description: "Finally, give your outfit a name and save it!",
+          },
+        },
+      ],
+    });
+
+    tour.drive();
+  }, 400);
 }
 
 export function goToNextTourStepOutfit() {
